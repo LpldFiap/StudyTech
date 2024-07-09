b2db088b3e75028ff62c45a111a8a2ad182b5fbd
GbrAntunes <gbr.antunes@gmail.com>
2024-07-09 11:57:18 -0300

    Routes config

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get("/posts")
+  getPosts() {
+    return this.appService.getHello("Get /posts");
+  }
+
+  @Get("/posts/:id")
+  getPost() {
+    return this.appService.getHello("Get /posts/:id");
+  }
+
+  @Post("/posts")
+  createPost() {
+    return this.appService.getHello("Post /posts");
+  }
+
+  @Put("/posts/:id")
+  updatePost() {
+    return this.appService.getHello("Put /posts/:id");
+  }
+
+  @Delete("/posts/:id")
+  deletePost() {
+    return this.appService.getHello("Delete /posts/:id");
+  }
+
+  @Get("/posts/search")
+  searchPost() {
+    return this.appService.getHello("Get /posts/search");
   }
 }
