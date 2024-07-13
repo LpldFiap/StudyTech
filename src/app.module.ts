ba6b70e7d718ed41cbb7552cd39b10f202691079
lucasdav <lucassouzadavanso@gmail.com>
2024-07-13 19:06:12 -0300

    documentação swagger e redoc api

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
 import { PostsModule } from './posts/posts.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [PostsModule, PostModule],
+  imports: [
+    PostsModule,
+    PostModule,
+    MongooseModule.forRoot(process.env.MONGO_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { setupRedoc } from './shared/middlewares/redoc.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('study-tech')
+    .setDescription('Study-tech API for FIAP tech challenger')
+    .setVersion('1.0')
+    .addTag('posts')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  // pode ser acessado a documentação em http://localhost:3010/api#/
+  SwaggerModule.setup('api', app, document);
+  // abaixo acessar a rota http://localhost:3010/docs
+  setupRedoc(app);
+
   await app.listen(3010);
 }
 bootstrap();
