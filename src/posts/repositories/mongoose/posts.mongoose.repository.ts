a4649d30fe3e74585434af5fc90bdb270f4aa6ea
PalomaCristina <paloma1899@gmail.com>
2024-07-22 23:22:59 -0300

    corrigindo a duplicidade de ids da rota POST posts/

--- src/posts/repositories/mongoose/posts.mongoose.repository.ts
+++ src/posts/repositories/mongoose/posts.mongoose.repository.ts
@@ -1,12 +1,12 @@
 import { InjectModel } from '@nestjs/mongoose';
 import { PostsRepository } from '../posts.repository';
 import { Model } from 'mongoose';
-import { Posts, PostDocument } from 'src/posts/schemas/posts.schema';
+import { Posts } from 'src/posts/schemas/posts.schema';
 import { IPosts } from 'src/posts/schemas/models/posts.interface';
 
 export class PostsMongooseRepository extends PostsRepository {
   constructor(
-    @InjectModel(Posts.name) private readonly postsModel: Model<PostDocument>,
+    @InjectModel(Posts.name) private readonly postsModel: Model<Posts>,
   ) {
     super();
   }
--- src/posts/schemas/posts.schema.ts
+++ src/posts/schemas/posts.schema.ts
@@ -1,14 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { IPosts } from './models/posts.interface';
-import mongoose, { HydratedDocument } from 'mongoose';
-
-export type PostDocument = HydratedDocument<Posts>;
 
 @Schema()
-export class Posts implements IPosts {
-  @Prop({ type: mongoose.Schema.Types.ObjectId, auto: true })
-  id: string;
-
+export class Posts {
   @Prop({ required: true })
   title: string;
 
